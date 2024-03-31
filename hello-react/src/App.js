cdf146dad52bdb50adca6686c5df21f796aaf3ae
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2024-03-31 19:15:17 +0000

    done

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,22 +1,65 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+const game = {
+  title: 'Bloodborne',
+  system: 'Playstation',
+  releaseDate: 'March 24, 2015',
+}
+const bosses =[
+  {id:'1', name:'Cleric Beast', isRequired: false},
+  {id:'2', name:'Father Gascoigne', isRequired: true},
+  {id:'3', name:'Vicar Amelia', isRequired: true},
+  {id:'4', name:'Witches of Hemwick', isRequired: false},
+  {id:'5', name:'Blood-starved Beast', isRequired: false}
+]
+
+function ShowGame(){
+  return(
+    <div>
+      <h2>{game.title}</h2>
+      <p>released: {game.releaseDate}</p>
+      <p>available on {game.system}</p>
+    </div>
+  
+  )
+}
+
+function ListBosses(){
+  const bossList = bosses.map( boss => 
+    <li 
+    key={bosses.id} 
+    style = {{
+      textDecorationLine: boss.isRequired ? 'underline' : 'none',
+      color: boss.isRequired ? 'green' : 'red'
+    }}
+    >
+      {boss.name}
+    </li>
+  );
+  return(
+    bossList
+  )
+}
+
+function ShowLegend(){
+  return(
+    <div style= {{background: 'grey',
+                  width: '200px'}}>
+      <h3>Legend</h3>
+      <p style= {{color: 'green'}}>required</p>
+      <p style= {{color: 'red'}}>optional</p>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <ShowGame/>
+       <ListBosses/>
+       <ShowLegend/>
       </header>
     </div>
   );
